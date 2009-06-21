085455a0fbd86fd27072444ac99b25df91f29e6f
Susanne Schmidt <tryshee@gmail.com>
2009-06-21 14:44:43 +0200

    initial commit

---
+++ alphademo.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Gtk2 '-init';
+use Cairo;
+
+my $supports_alpha;
+
+sub expose {
+    my($widget, $event) = @_;
+    my($width, $height, $radius);
+    
+    my $cr = Gtk2::Gdk::Cairo::Context->create($widget->window());
+
+    if($supports_alpha) {
+        $cr->set_source_rgba(1.0, 1.0, 1.0, 0.0); 
+    } else {
+        $cr->set_source_rgb(1.0, 1.0, 1.0);
+    } 
+
+    $cr->set_operator('source');
+    $cr->paint();
+    $cr->set_source_rgba(1.0, 0.2, 0.2, 0.6);
+
+    ($width, $height) = $widget->get_size();
+
+    if($width < $height) {
+        $radius = $width/2 - 0.8;
+    } else {
+        $radius = $height/2 - 0.8;
+    }
+
+    $cr->arc($width/2, $height/2, $radius, 0, 2.0*3.14);
+    $cr->fill();
+    $cr->stroke();
+}
+
+sub screen_changed {
+    my ($widget, $old_screen) = @_;
+
+    my $screen   = $widget->get_screen();
+    my $colormap = $screen->get_rgba_colormap();
+
+    if(!$colormap){ 
+        print "Your screen doesn't support alpha channels!\n";
+        $colormap = $screen->get_rgb_colormap();
+    } else {
+        $supports_alpha = 1;
+        print "Your screen does support alpha channels!\n";
+    }
+
+    $widget->set_colormap($colormap);
+}
+
+sub main {
+    my $window = Gtk2::Window->new();
+    
+    $window->set_title("Alpha Demo");
+    $window->set_app_paintable('TRUE');
+
+    $window->signal_connect('delete_event', \&quit);
+    $window->signal_connect('expose_event', \&expose, $window);
+    $window->signal_connect('screen_changed', \&screen_changed, $window);
+    
+    screen_changed($window);
+
+    $window->show_all();
+    Gtk2->main;
+}
+
+sub quit {
+    exit;
+}
+
+main();
---
+++ gtktwitter.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Gtk2 '-init';
+use Net::Twitter;
+
+my $username = 'FOO';
+my $password = 'BAR';
+
+my $window  = Gtk2::Window->new;
+
+$window->signal_connect('delete_event' => sub { Gtk2->main_quit; } );
+
+$window->set_title('glipsch twitter client');
+$window->set_default_size('600', '80');
+$window->set_border_width('10');
+
+my $mainbox   = Gtk2::VBox->new('TRUE', 5);
+my $inputbox  = Gtk2::VBox->new('TRUE', 10);
+my $updatebox = Gtk2::HBox->new('TRUE', 0);
+
+my $input     = Gtk2::Entry->new_with_max_length('140');
+my $counter   = Gtk2::Label->new();
+my $update    = Gtk2::Button->new('update');
+
+$counter->set_alignment('0.1', '0.5');
+
+$input->signal_connect('expose_event' => sub {
+                       my ($widget, $event) = @_;
+                       on_expose($widget, $event, $counter);
+});
+
+$input->signal_connect('activate' => sub { 
+                       my ($widget, $event) = @_;
+                       on_enter($widget, $event, $update); 
+});
+
+$update->signal_connect('clicked' => sub {
+                        my ($widget, $event) = @_;
+                        on_click($widget, $event, $input);
+});
+
+$inputbox->pack_start($input, 'FALSE', 'FALSE', 0);
+$updatebox->pack_start($counter, 'FALSE', 'FALSE', 30);
+$updatebox->pack_start($update, 'FALSE', 'FALSE', 0);
+
+$mainbox->pack_start($inputbox, 'FALSE', 'FALSE', 0);
+$mainbox->pack_start($updatebox, 'FALSE', 'FALSE', 3);
+
+$window->add($mainbox);
+$window->show_all;
+
+Gtk2->main;
+
+sub on_enter {
+    my ($widget, $event, $button) = @_;
+    $button->clicked();
+}
+
+sub on_click {
+    my ($widget, $event, $input) = @_;
+    my $message = $input->get_text();
+     
+    my $twit = Net::Twitter->new( {'username' => "$username",
+                                   'password' => "$password", } );
+
+    my $status = $twit->update($message);
+
+    if($status) {
+        empty_input_on_success(@_);
+    }
+}
+
+sub empty_input_on_success {
+    my ($widget, $event, $input) = @_;
+    sleep(3);
+    $input->set_text("");
+    $input->show;
+}
+
+sub on_expose {
+    my ($widget, $event, $label) = @_;
+    my $charcount                = length($input->get_text());
+
+    $label->set_text($charcount);
+    $label->set_markup("<big><b><span foreground=\"#dd00dd\">$charcount</span></b></big>");
+    $label->show;
+}
+
+1;
